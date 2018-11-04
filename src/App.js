bac5fb744b92db0d2b81629f12778af5eaeb0968
mihalskiy <mihalskiy.rv@gmail.com>
2018-11-05 00:21:11 +0200

    add navigate

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
 import Clock from './component/Clock';
 import Header from './component/Header';
+import NavigationIcon from '@material-ui/icons/Navigation';
+import Button from '@material-ui/core/Button';
+import Icon from '@material-ui/core/Icon';
+import red from '@material-ui/core/colors/red';
+
 
 class App extends Component {
   render() {
@@ -14,6 +19,15 @@ class App extends Component {
                   <div className="top-right">Top Right</div>
                   <div className="bottom-right">Bottom Right</div>*/}
                   <div className="centered">
+                      <Button variant="extendedFab" aria-label="Delete" className="button">
+                          НАШИ РОБОТЫ
+                      </Button>
+                      <Button variant="extendedFab" aria-label="Delete" className="button">
+                          НАНИМАЙТЕ НАС, ЧТОБЫ ПОМОЧЬ
+                      </Button>
+                      <Button variant="extendedFab" aria-label="Delete" className="button">
+                          ЗАКАЖИТЕ ОДИН ИЗ НАШИХ ПАКЕТОВ
+                      </Button>
                   </div>
           </div>
         <Clock/>
