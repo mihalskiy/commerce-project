9507ccda14e062012cc029d93d0adfbfb19bed6c
mihalskiy <mihalskiy.rv@gmail.com>
2018-11-04 23:32:14 +0200

    add navigate

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
-import Header from './component/Header';
 import Clock from './component/Clock';
+import Header from './component/Header';
 
 class App extends Component {
   render() {
     return (
       <div>
-          <Header name="header" />
+          <Header />
+          <div className="img-with-item">
+              <img src={require("./img/welcome.jpg")} alt="Snow" />
+                  {/*<div className="bottom-left">Bottom Left</div>
+                  <div className="top-left">Top Left</div>
+                  <div className="top-right">Top Right</div>
+                  <div className="bottom-right">Bottom Right</div>*/}
+                  <div className="centered">
+                  </div>
+          </div>
         <Clock/>
       </div>
     );
