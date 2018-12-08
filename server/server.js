d7a80418f777eccc65b4d8170f7d1c3cd61d4d87
mihalskiy <mihalskiy.rv@gmail.com>
2018-12-08 19:36:48 +0200

    fix err

--- server/server.js
+++ server/server.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
-const cors = require('cors')
-
 
 const app = express();
 app.use(logger('dev'));
