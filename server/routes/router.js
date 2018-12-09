60b3075f585138c4f8eb249ca1d96b7a6fdaafa6
mihalskiy <mihalskiy.rv@gmail.com>
2018-12-09 08:37:58 +0200

    fix docker config

--- server/routes/router.js
+++ server/routes/router.js
@@ -1,16 +1,14 @@
 const ordersController = require('../controllers').orders;
 const orderItemsController = require('../controllers').orderItems;
 require('dotenv').config();
-var cors = require('cors')
-
 
 module.exports = (app) => {
   app.get('/api', (req, res) => res.status(200).send({
     message: process.env.ROOT_USER + '- test',
   }));
 
-  app.post('/api/orders', cors(), ordersController.create);
-  app.get('/api/orders', cors(), ordersController.list);
+  app.post('/api/orders', ordersController.create);
+  app.get('/api/orders', ordersController.list);
   app.get('/api/order/:id', ordersController.retrieve);
   app.put('/api/order/:id', ordersController.update);
   app.delete('/api/order/:id', ordersController.destroy);
--- server/server.js
+++ server/server.js
@@ -11,7 +11,8 @@ app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Headers", "Origin,Content-Type, Authorization, x-id, Content-Length, X-Requested-With");
     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
     next();
-});app.use(bodyParser.urlencoded({ extended: false }));
+});
+app.use(bodyParser.urlencoded({ extended: false }));
 
 require('./routes/router')(app);
 app.get('*', (req, res) => res.status(200).send({
