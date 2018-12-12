ccae4dbc653d659b8bc8a7d747c030f29a2e2c10
mihalskiy <mihalskiy.rv@gmail.com>
2018-12-12 21:40:49 +0200

    fix config

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://127.0.0.1:80");
+    res.header("Access-Control-Allow-Origin", process.env.PUBLIC_URL);
     res.header("Access-Control-Allow-Credentials", "true");
     res.header("Access-Control-Allow-Headers", "Origin,Content-Type, Authorization, x-id, Content-Length, X-Requested-With");
     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
